f76228458729f5029ca6e5d5f4b67318fd0f1a8f
sedat23 <sukumar.reddy.vangala@turner.com>
2019-12-11 10:56:04 -0500

    Solution along with test cases for Q: 'Write a function that returns the sum of two numbers.'

---
+++ intro/add-1/add.js
@@ -0,0 +1,9 @@
+/* Question : Write a function that returns the sum of two numbers. */ 
+
+// URL : https://app.codesignal.com/arcade/intro/level-1/jwr339Kq6e3LQTsfa 
+
+function add(param1, param2) {
+    return param1 + param2;
+}
+
+exports.add = add;
