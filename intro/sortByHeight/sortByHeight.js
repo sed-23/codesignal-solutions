dd36ce77708179c91b457898fcc5dc4ff1ae40cf
sedat23 <sukumar.reddy.vangala@turner.com>
2020-02-18 15:45:22 -0500

    Solution for Q: https://app.codesignal.com/arcade/intro/level-3/D6qmdBL2NYz49XHwM

---
+++ intro/sortByHeight/sortByHeight.js
@@ -0,0 +1,10 @@
+/* Q: Some people are standing in a row in a park. There are trees between
+      them which cannot be moved. Your task is to rearrange the people by their
+      heights in a non-descending order without moving the trees. People can be very tall! */
+
+// URL : https://app.codesignal.com/arcade/intro/level-3/D6qmdBL2NYz49XHwM
+
+function sortByHeight(a) {
+    let sorted = a.filter(elem => elem > 0).sort((el1, el2) => el1 - el2);
+    return a.map(el => el > 0 ? sorted.shift() : -1 )
+}
