58ae2e3d83babe92e05f33f5c7e17f9c10584055
sedat23 <sukumar.reddy.vangala@turner.com>
2020-02-19 11:07:11 -0500

    Solution for Q: https://app.codesignal.com/arcade/intro/level-3/9DgaPsE2a7M6M2Hu6

---
+++ intro/reverseInParentheses-13/reverseInParentheses.js
@@ -0,0 +1,12 @@
+/* Q: Write a function that reverses characters in (possibly nested) parentheses in the input string.
+      Input strings will always be well-formed with matching ()s. */
+
+// URL : https://app.codesignal.com/arcade/intro/level-3/9DgaPsE2a7M6M2Hu6
+
+function reverseInParentheses(inputString) {
+    if (inputString.match(/\([a-z]*\)/)) {
+        return reverseInParentheses(inputString.replace(/\([a-z]*\)/, 
+            Array.from(inputString.match(/\([a-z]*\)/)[0].replace(/\(|\)/g,'')).reverse().join('')));
+    }
+    else return inputString;
+}
