3eb2b65dda652485e0f10d3d1a01599210c09e75
sedat23 <sukumar.reddy.vangala@turner.com>
2020-02-19 15:06:27 -0500

    Solution for Q: https://app.codesignal.com/arcade/intro/level-4/ZCD7NQnED724bJtjN

---
+++ intro/addBorder/addBorder.js
@@ -0,0 +1,9 @@
+/* Question : Given a rectangular matrix of characters, add a border of asterisks(*) to it. */ 
+
+// URL : https://app.codesignal.com/arcade/intro/level-4/ZCD7NQnED724bJtjN
+
+function addBorder(picture) {
+    picture.push('*'.repeat(picture[0].length))
+    picture.unshift('*'.repeat(picture[0].length))
+    return picture.map(el => `*${el}*`)
+}
