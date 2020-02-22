507671df0369c47e36441ec057dd1b879fc23779
sedat23 <sukumar.reddy.vangala@turner.com>
2020-02-22 12:27:51 -0500

    Test Cases for Q: https://app.codesignal.com/arcade/intro/level-3/D6qmdBL2NYz49XHwM

---
+++ intro/sortByHeight-12/sortByHeight.spec.js
@@ -0,0 +1,13 @@
+var assert = require('assert');
+var sortByHeight = require('./sortByHeight').sortByHeight;
+
+describe('Sort By Height.', function(){
+    function testCondition(input, res) {
+        it(`${res} is sorted by height.`, function() {
+            assert.equal(sortByHeight(input), res); 
+        });
+    }
+    let testCases = [[[-1, 150, 190, 170, -1, -1, 160, 180], [-1, 150, 160, 170, -1, -1, 180, 190]],
+                      [[-1, -1, -1, -1, -1], [-1, -1, -1, -1, -1]], [[-1], [-1]]];
+    testCases.forEach(element => testCondition(...element));
+});
