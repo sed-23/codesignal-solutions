d4f13bd04c248d10f2b9da0f4801f41a577f47ae
sed-23 <sukumarvangala@gmail.com>
2021-02-12 14:20:16 -0500

    Solution for https://app.codesignal.com/arcade/intro/level-5/veW5xJednTy4qcjso

--- intro/20-arrayMaximalAdjacentDifference/arrayMaximalAdjacentDifference.js
+++ intro/20-arrayMaximalAdjacentDifference/arrayMaximalAdjacentDifference.js
@@ -7,5 +7,4 @@ function arrayMaximalAdjacentDifference(inputArray) {
     return Math.max(...inputArray.slice(1).map((x,i)=>Math.abs(x-inputArray[i])))
 }
 
-
 exports.arrayMaximalAdjacentDifference = arrayMaximalAdjacentDifference;
