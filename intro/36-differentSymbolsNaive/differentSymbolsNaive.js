3cb1a5c3297df66cbe1279e6e4881fc39b2689ad
Sid Reddy <sukumarreddy.v5@gmail.com>
2022-11-01 18:15:19 -0400

    Added solutions for 36 and 37

---
+++ intro/36-differentSymbolsNaive/differentSymbolsNaive.js
@@ -0,0 +1,10 @@
+/* Q: Given a string, find the number of different characters in it. */
+
+// URL : https://app.codesignal.com/arcade/intro/level-8/8N7p3MqzGQg5vFJfZ
+
+function differentSymbolsNaive(inputArray, k) {
+    return (new Set(s.split(""))).size
+}
+
+
+exports.differentSymbolsNaive = differentSymbolsNaive;
