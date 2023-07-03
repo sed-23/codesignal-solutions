eada60d568960617c79f0204bcebb71ec2fea3f9
Sid Reddy <sukumarreddy.v5@gmail.com>
2023-07-03 15:37:38 -0400

    Added 5 sols from core

---
+++ the_core/02-largestNumber/largestNumber.js
@@ -0,0 +1,9 @@
+/* Question : Given an integer n, return the largest number that contains exactly n digits. */ 
+
+// URL : https://app.codesignal.com/arcade/code-arcade/intro-gates/SZB5XypsMokGusDhX
+
+function largestNumber(n) {
+    return 10 ** n - 1
+}
+
+exports.largestNumber = largestNumber;
