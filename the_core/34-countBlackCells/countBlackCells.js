2187ca3a838195c5c3ee3306d43f75bb4e417708
sed <36825830+sed-23@users.noreply.github.com>
2023-07-13 10:06:07 -0400

    Arcade (#29)
    * Renamed files to simplify search.
    
    * Solution and test cases for Q: https://app.codesignal.com/arcade/intro/level-4/xvkRbxYkdHdHNCKjg/drafts
    
    * Travis CI initial setup.
    
    * Solution and test cases for Q: https://app.codesignal.com/arcade/intro/level-4/Xfeo7r9SBSpo3Wico
    
    * Solution for https://app.codesignal.com/arcade/intro/level-5/g6dc9KJyxmFjB98dL
    
    * Solution for https://app.codesignal.com/arcade/intro/level-5/EEJxjQ7oo7C5wAGjE
    
    * Solution for https://app.codesignal.com/arcade/intro/level-5/veW5xJednTy4qcjso
    
    * Solution for https://app.codesignal.com/arcade/intro/level-5/XC9Q2DhRRKQrfLhb5
    
    * BoxBlur - Solution and test cases for Q:https://app.codesignal.com/arcade/intro/level-5/5xPitc3yT3dqS7XkP
    
    * MineSweeper - Solution and test cases for Q:https://app.codesignal.com/arcade/intro/level-5/ZMR5n7vJbexnLrgaM
    
    * arrayReplace - Solution and test cases for Q:https://app.codesignal.com/arcade/intro/level-6/mCkmbxdMsMTjBc3Bm
    
    * Sol and Test cases for Q: https://app.codesignal.com/arcade/intro/level-6/6cmcmszJQr6GQzRwW
    
    * Sol for Q: https://app.codesignal.com/arcade/intro/level-6/6Wv4WsrsMJ8Y2Fwno
    
    * Renaming to fix the display order across platforms.
    
    * Added solutions for 28 and 29.
    
    * Added solutions for stringsRearrangement and absoluteValuesSumMinimization.
    
    * Solutions for 34 and 35 in arcade.
    
    * Added solutions for 36 and 37
    
    * Added 3 solutions for arcade questions 38, 39 and 40
    
    * Added solutions for digitDegree-arcade question
    
    * Added solutions for arcade questions 42-45
    
    * Added solutions for arcade problems for questions 46-50
    
    * Adding solutions for arcade questions 51-55
    
    * Solutions for 56-60
    
    * Added 5 sols from core
    
    * added sols for 06-10 in core module
    
    * Added sols for problems from 11-15 in core section.
    
    * Added sols for the problems 15-20 in the core section.
    
    * Added sols for 21-25 in the core section.
    
    * updated sols for 26-30 in the core section.
    
    * Added sols for 31-35 in the core section
    
    ---------
    
    Co-authored-by: Sid Reddy <sukumarreddy.v5@gmail.com>

---
+++ the_core/34-countBlackCells/countBlackCells.js
@@ -0,0 +1,12 @@
+/* Question : You have k apple boxes full of apples. Each square box of size m contains m × m apples. You just noticed two interesting properties about the boxes:
+
+            The smallest box is size 1, the next one is size 2,..., all the way up to size k.
+            Boxes that have an odd size contain only yellow apples. Boxes that have an even size contain only red apples.
+            Your task is to calculate the difference between the number of red apples and the number of yellow apples. */ 
+
+// URL : https://app.codesignal.com/arcade/code-arcade/loop-tunnel/scG8AFsPuqQGx8Qjf
+
+
+function countBlackCells(k) {
+    return k * (k + 1) / (k % 2 ? -2 : 2);
+}
